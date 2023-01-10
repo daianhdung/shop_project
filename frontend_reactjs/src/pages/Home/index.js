68bf0b99a79aaa7339c902753b28f5dc8c3d336c
daianhdung <daianhdung99@gmail.com>
2023-01-10 18:06:02 +0700

    add slider homepage

--- frontend_reactjs/src/pages/Home/index.js
+++ frontend_reactjs/src/pages/Home/index.js
@@ -1,5 +1,34 @@
+import "swiper/css/bundle";
+import classNames from 'classnames/bind';
+
+
+import "~/components/MySwiper/style.css";
+
+import styles from './Home.module.scss';
+import React from "react";
+import ShoesSwiper from "~/components/MySwiper/ShoesSwiper";
+
+const cx = classNames.bind(styles);
+
 function Home() {
-    return <h2 >Home page</h2>;
+    return (<React.Fragment>
+        <div className={cx('separate')}>
+            <h1>Thương hiệu nổi bật</h1>
+            <div >
+                <div className={cx('wrap_swiper')}>
+                    <div className={cx('top_branch_slide')}><ShoesSwiper /></div>
+                </div>
+            </div>
+        </div>
+        <div className={cx('separate')}>
+            <h1>Top bán chạy</h1>
+            <div className={cx('wrapper')}>
+                <div className={cx('wrap_swiper')}>
+                    <div className={cx('top_branch_slide')}><ShoesSwiper /></div>
+                </div>
+            </div>
+        </div>
+    </React.Fragment>);
 }
 
 export default Home;
